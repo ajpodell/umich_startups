d3c77a4cbe4f92d563e385f86a2a2b536a05dcaa
apodes <aaronpodell@gmail.com>
2013-07-16 16:56:32 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UmichStartups::Application.config.secret_token = '028cc1341e2eba0f2382c25248a707db3818b0a20ea767743b94e83646c66fa7115cfba4367f1f00ce536bb9dd0a9511423813fae16d19dd34f558d02b258ccf'
